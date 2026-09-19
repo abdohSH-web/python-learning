125c5f63fc05c77cbbbc80deb41a706b29d26719
SkillFarm <wwwabdalla774656373@gmail.com>
2026-09-19 21:07:22 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,5 +1,25 @@
-course = 'python'
-level = 'beginner'
-teacher = 'Abdullah'
-display = ['__', k==6]
-print(display)
+import os
+import random
+
+def clear_screen():
+    # os.name بتكون 'nt' لو النظام ويندوز، وغير كذا تكون 'posix' (ماك/لينكس)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# توليد رقم عشوائي بين 1 و 10
+secret_number = random.randint(1, 10)
+
+while True:
+    try:
+        guess = int(input('Guess a number between 1 and 10'))
+        
+        if guess == secret_number:
+            print("Corret! You won")
+            break
+        else:
+            # مسح الشاشة قبل التكرار التالي
+            clear_screen()
+            print("Wrong! Try again\n")
+            
+    except ValueError:
+        clear_screen()
+        print("Please enter a valid number!\n")
